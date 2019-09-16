8a6cac8f5ced32144ac78f0f337419687cea56f5
SeaCow7 <daniel.marker@gmail.com>
2019-09-16 22:14:27 +0100

    Removed standard assets stuff. Recreated scripts folder and ThirdPersonController.cs. Also updated prefab for ThirdPersonController.

---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+
+    [SerializeField] float sphereRadius = 2f;
+    [SerializeField] [Range(0f, 5f)] float sensitivity = 1f;
+    float sphereAngleX = 0f;
+    float sphereAngleY = 60f;
+    Camera camera;
+    Vector3 initCameraPos;
+
+    void Start()
+    {
+        camera = Camera.main;
+        initCameraPos = camera.transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float rawVertical = Input.GetAxis("Vertical");
+        float rawHorizontal = Input.GetAxis("Horizontal");
+
+        float rawMouseX = Input.GetAxis("Mouse X");
+        float rawMouseY = Input.GetAxis("Mouse Y");
+
+        print("Mouse X: " + rawMouseX);
+        print("Mouse Y: " + rawMouseY);
+
+        //using mouseX and mouseY, move the camera about a sphere around the player with radius sphereRadius
+        Vector3 newPos = new Vector3();
+
+        sphereAngleX -= rawMouseY * sensitivity;
+        sphereAngleY += rawMouseX * sensitivity;
+
+        newPos.x = sphereRadius * Mathf.Sin(Mathf.Deg2Rad * sphereAngleX) * Mathf.Sin(Mathf.Deg2Rad * sphereAngleY);
+        newPos.y = sphereRadius * Mathf.Cos(Mathf.Deg2Rad * sphereAngleX);
+        newPos.z = sphereRadius * Mathf.Sin(Mathf.Deg2Rad * sphereAngleX) * Mathf.Cos(Mathf.Deg2Rad * sphereAngleY);
+
+        camera.transform.position = newPos;
+        camera.transform.rotation = Quaternion.LookRotation(transform.position - camera.transform.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, sphereRadius);
+    }
+}
