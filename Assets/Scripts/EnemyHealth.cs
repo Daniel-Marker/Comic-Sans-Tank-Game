d126ac69c07e60b64873e4e2eb586b1da12f6381
SeaCow7 <daniel.marker@gmail.com>
2019-09-18 16:19:49 +0100

    Created a PlayerHealth script which updates the UI to show the current player health. Created EnemyHealth to show the health of an enemy above the enemies head. Also added a shader and font so that the text isn't visible through walls. Created an empty script for Enemy AI. Created a prefab for the enemy and a blue material for them. Finally, made a todo list.

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float maxHealth;
+    [SerializeField] TextMesh healthText;
+    float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthText();
+    }
+
+    public void DecreaseHealth(float amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth < 0)
+        {
+            HandleEnemyDeath();
+        }
+
+        UpdateHealthText();
+    }
+
+    public void IncreaseHealth(float amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        UpdateHealthText();
+    }
+
+    private void HandleEnemyDeath()
+    {
+        print(gameObject.name + " has died");
+    }
+
+    void UpdateHealthText() {
+        healthText.text = "HP: " + currentHealth + "/" + maxHealth;
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float maxHealth;
+    [SerializeField] Text healthText;
+    float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthText();
+    }
+
+    public void DecreaseHealth(float amount) {
+        currentHealth -= amount;
+
+        if (currentHealth < 0)
+        {
+            HandlePlayerDeath();
+        }
+
+        UpdateHealthText();
+    }
+
+    public void IncreaseHealth(float amount) {
+        currentHealth += amount;
+
+        if (currentHealth > maxHealth) {
+            currentHealth = maxHealth;
+        }
+
+        UpdateHealthText();
+    }
+
+    private void HandlePlayerDeath()
+    {
+        print("Dead");
+    }
+
+    private void UpdateHealthText() {
+        healthText.text = "HP: " + currentHealth + "/" + maxHealth;
+    }
+}
