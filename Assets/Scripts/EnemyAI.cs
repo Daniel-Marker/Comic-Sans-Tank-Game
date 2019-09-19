1096715bf7618215a7436d9d10b3bea4083251b2
SeaCow7 <daniel.marker@gmail.com>
2019-09-19 13:24:31 +0100

    Updated EnemyAI so that the tank head faces the player if they are in view.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public enum EnemyType { stationary, basic };
+
+    [Header("Enemy properties")]
+    [SerializeField] EnemyType enemyType;
+    [SerializeField] float timeBetweenShots = 2f;
+
+    [Header("Enemy parts")]
+    [SerializeField] GameObject cannon;
+    [SerializeField] GameObject tankHead;
+    [SerializeField] GameObject tankBody;
+
+    ThirdPersonController player;
+
     void Start()
     {
-        
+        player = FindObjectOfType<ThirdPersonController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!Physics.Linecast(transform.position, player.transform.position)) {
+            tankHead.transform.up = player.transform.position - transform.position;
+            tankHead.transform.rotation = Quaternion.LookRotation(player.transform.position - transform.position);
+            Vector3 currRot = tankHead.transform.rotation.eulerAngles;
+            currRot.x += 90f;
+            currRot.z = 0;
+            tankHead.transform.rotation = Quaternion.Euler(currRot);
+        }
     }
 }
