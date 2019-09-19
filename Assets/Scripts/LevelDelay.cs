a07c3989d190218d195fe91f40e7d3eb9b1f018f
SeaCow7 <daniel.marker@gmail.com>
2019-09-19 22:43:41 +0100

    Created LevelDelay so that before each level there is a countdown

---
+++ Assets/Scripts/LevelDelay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelDelay : MonoBehaviour
+{
+    [SerializeField] int delay = 3;
+    [SerializeField] Text countDownText;
+
+    EnemyAI[] enemies;
+    ThirdPersonController player;
+
+
+    void Start()
+    {
+        enemies = FindObjectsOfType<EnemyAI>();
+        foreach (EnemyAI enemy in enemies) {
+            enemy.enabled = false;
+        }
+
+        player = FindObjectOfType<ThirdPersonController>();
+        player.enabled = false;
+
+        StartCoroutine(Countdown(delay));
+    }
+
+    IEnumerator Countdown(int delay)
+    {
+        while (delay != 0) {
+            countDownText.text = delay + "!";
+            yield return new WaitForSeconds(1f);
+            delay--;
+        }
+
+        foreach (EnemyAI enemy in enemies)
+        {
+            enemy.enabled = true;
+        }
+
+        player.enabled = true;
+        countDownText.enabled = false;
+    }
+}
